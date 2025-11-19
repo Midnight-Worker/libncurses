5ba784f3fb89d536909daf5ed26d7d3ce061b345
EyeBallPaul <maik.tappe82@googlemail.com>
2025-11-19 06:05:14 +0100

    horeload für mingw msys2 auf windows

--- hotreload.sh
+++ hotreload.sh
@@ -1,3 +1,6 @@
+# sudo apt install inotify-tools
+# pacman -S mingw-w64-x86_64-fswatch
+
 while inotifywait -q -e close_write main.c; do
     clear
     echo "=== Kompiliere... ==="
@@ -10,3 +13,23 @@ while inotifywait -q -e close_write main.c; do
     echo
     echo "=== Fertig, warte auf nächste Änderung... ==="
 done
+
+
+#### Mit fswatch ####
+#!/bin/bash
+
+#clear
+#echo "Watching test.py..."
+
+#fswatch -o test.py | while read num; do
+#    clear
+#    echo "=== Kompiliere... ==="
+#    if gcc main.c -o prog; then
+#        echo "=== Starte prog ==="
+#        ./prog
+#    else
+#        echo "Kompilierfehler 😭"
+#    fi
+#    echo
+#    echo "=== Fertig, warte auf nächste Änderung... ==="
+#done
